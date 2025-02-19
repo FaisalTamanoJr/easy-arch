bdf300315d4e8c505d9b31e70a5fdf4f12d461be
FaisalRichardJr <faisalrichardjr@gmail.com>
2025-02-20 04:05:17 +0800

    changes

--- InstallArchFast2.sh
+++ InstallArchFast2.sh
@@ -21,11 +21,7 @@ curl $wallpaper --output Pictures/wallpaper.jpg
 
 # Clone the awesome window manager config
 git clone https://github.com/FaisalTamanoJr/project-dotfiles.git .awesomeConfig
-if [ -d ".config/awesome" ]; then
-    rm -rf .config/awesome
-fi
-mkdir .config/awesome
-cp -a .awesomeConfig/.config/awesome .config/awesome
+cp -a .awesomeConfig/.config/awesome .config/
 
 
 # Start the enabled services
